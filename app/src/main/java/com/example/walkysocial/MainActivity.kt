c0629d186b155b669be581a655f871eb5bd0ac87
paulingaillot <paulingaillot@gmail.com>
2023-03-08 14:54:33 -0500

    Liste Appareil Bluetooth Proximite

--- app/src/main/java/com/example/walkysocial/MainActivity.kt
+++ app/src/main/java/com/example/walkysocial/MainActivity.kt
@@ -10,11 +10,9 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.walkysocial.Services.BluetoothService
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
