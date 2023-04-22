28f2f648d2a86226237cc2fc5f9769ff2ab76345
paulingaillot <paulingaillot@gmail.com>
2023-04-22 11:45:37 -0400

    FireBase Auth

--- app/src/main/java/fr/isen/walkysocial/Activities/Inscription.kt
+++ app/src/main/java/fr/isen/walkysocial/Activities/Inscription.kt
@@ -9,6 +9,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import fr.isen.walkysocial.MainActivity
@@ -29,8 +31,9 @@ class Inscription : AppCompatActivity() {
             var username = findViewById<EditText>(R.id.editTextTextPersonName)
             var radiogroup = findViewById<RadioGroup>(R.id.radiogroup)
             var password = findViewById<EditText>(R.id.password)
+            var email = findViewById<EditText>(R.id.email)
 
-            if(radiogroup.checkedRadioButtonId != 0 && username.text.isNotEmpty()) {
+            if(radiogroup.checkedRadioButtonId != 0 && username.text.isNotEmpty() && email.text.isNotEmpty() &&password.text.isNotEmpty()) {
                 var radiobutcheck = findViewById<RadioButton>(radiogroup.checkedRadioButtonId)
 
                 var userClass = UserClass.TANK;
@@ -45,17 +48,29 @@ class Inscription : AppCompatActivity() {
                         userClass = UserClass.MAGE
                 }
 
-                var user = User(username.text.toString(), userClass , password.text.toString())
+                var auth = Firebase.auth
+                auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign up success, update UI with the created user's information
+                            Log.d("test", "createUserWithEmail:success")
+                            var user = User(username.text.toString(), userClass, auth.currentUser!!.uid)
 
-                val db = Firebase.firestore
-                var profils = db.collection("user")
+                            val db = Firebase.firestore
+                            var profils = db.collection("user")
 
-                profils.add(user).addOnCompleteListener {
-                    MainActivity.user = user
-                    val main = Intent(applicationContext, MainActivity::class.java)
-                    startActivity(main)
-                    finish()
-                }
+                            profils.add(user).addOnCompleteListener {
+                                MainActivity.user = user
+                                val main = Intent(applicationContext, MainActivity::class.java)
+                                startActivity(main)
+                                finish()
+                            }
+                        } else {
+                            // If sign up fails, display a message to the user.
+                            Log.w("test", "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(baseContext, "Failed to create user.",Toast.LENGTH_SHORT).show()
+                        }
+                    }
 
 
 
